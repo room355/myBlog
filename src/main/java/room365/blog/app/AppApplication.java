a45727a8fef38136e822c0a863707573f4b31831
LBR2017 <30385239+LBR2017@users.noreply.github.com>
2020-12-05 18:22:57 -0500

    deploy as war changes, allow deployment to azure webapps

--- src/main/java/room365/blog/app/AppApplication.java
+++ src/main/java/room365/blog/app/AppApplication.java
@@ -2,12 +2,20 @@ package room365.blog.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
 }
+
